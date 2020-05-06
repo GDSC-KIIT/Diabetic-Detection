16540b9384f0bc801f6038b35095635902422ba6
sayannath <sayannath235@gmail.com>
2020-05-07 00:07:51 +0530

    Add

---
+++ android/app/src/main/kotlin/com/example/diabetic_detection/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.diabetic_detection
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
